9e72b25f03f0597ee31dafcb03d25d6da3394240
Lueur <sgdm515@gmail.com>
2026-03-28 22:54:54 +0300

    models file

--- src/models.py
+++ src/models.py
@@ -27,7 +27,7 @@ class Task:
         return f"Task: {self.title}\nDescription: {self.description}\nStatus: {self.is_done}\n"
 
     def __str__(self) -> str:
-        return f"Task: {self.title} Status: {self.is_done}"
+        return f"Task: {self.title} | Status: {self.is_done}"
 
     def __repr__(self) -> str:
         return f"\nTask: {self.title}\nDescription: {self.description}\nStatus: {self.is_done}"
